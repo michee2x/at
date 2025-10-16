8eff9ac6ae23251da092b033707620c003a645a3
michee2x <michaelisraelmike@gmail.com>
2025-10-16 20:46:43 +0100

    fix: refixed

--- components/BlurredDots.tsx
+++ components/BlurredDots.tsx
@@ -3,14 +3,13 @@ import React, { memo } from "react";
 const BlurredDots = memo(({ className }: { className: string }) => {
   return (
     <div className={`flex flex-nowrap gap-1.5 top-0 ${className} absolute`}>
-      {[...Array(28)].map((x, i) => (
-        <div
-          key={`${i}${x}`}
-          className={`w-[4.5px] h-[4.5px] bg-white/80 rounded-full`}
-        />
+      {[...Array(28)].map((_, i) => (
+        <div key={i} className="w-[4.5px] h-[4.5px] bg-white/80 rounded-full" />
       ))}
     </div>
   );
 });
+
+BlurredDots.displayName = "BlurredDots"; // 👈 fixes ESLint react/display-name
 
 export default BlurredDots;
--- components/HeadPhoneGrid.tsx
+++ components/HeadPhoneGrid.tsx
@@ -1,49 +1,7 @@
 "use client";
 import { Headphone, headphones } from "@/constants";
-import Image from "next/image";
-import { memo } from "react";
-import BlurredDots from "./BlurredDots";
+import HeadphoneCard from "./HeadPhoneCard";
 
-const HeadphoneCard = memo(
-  ({ item }: { item: Headphone }) => {
-    return (
-      <div className="bg-gradient-to-b relative pt-4 from-[#6A00EF] to-[#DBBFFF] flex flex-col place-content-between w-full h-[304.59px] hover:z-50 overflow-hidden text-center text-white hover:scale-105 transition-transform duration-300">
-        <div className="flex-1 relative">
-          <h1 className="lg:text-[18px] text-[15px] w-full h-auto items-center">
-            {item.name}
-          </h1>
-          <div className="flex justify-center">
-            <Image
-              src={item.image}
-              alt={item.name}
-              width={120}
-              height={120}
-              className="object-contain"
-            />
-          </div>
-          <ul className="text-xs mt-8 list text-gray-300 space-y-1">
-            {item.features.map((feature, i) => (
-              <li
-                key={`${i}`}
-                className="flex items-center justify-center gap-2 text-gray-100"
-              >
-                <span className="w-1.5 h-1.5 bg-white rounded-full"></span>
-                <span>{feature}</span>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="h-[36px] relative w-full flex items-center justify-center text-black text-[18.94px] bg-white">
-          {item.price}
-        </div>
-        <BlurredDots className="flex-col" />
-        <BlurredDots className="right-0 flex-col" />
-        <BlurredDots className="top-0" />
-      </div>
-    );
-  },
-  (prevProps, nextProps) => prevProps.item.name === nextProps.item.name
-);
 
 export default function HeadphoneGrid() {
   return (
