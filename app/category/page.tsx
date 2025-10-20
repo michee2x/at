d2028908c259e4fb9d68ad07cd5fb863844834a4
michee2x <michaelisraelmike@gmail.com>
2025-10-20 18:10:14 +0100

    latest commit

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -10,6 +10,7 @@ import CategoryFilters from "@/components/CategoryFilters";
 import ProductNotFound from "@/components/lottie/ProductNotFound";
 import { FaStar } from "react-icons/fa";
 import { GoPlus } from "react-icons/go";
+import { WooProduct } from "@/types";
 
 
 const skeletonArray = Array.from({ length: 8 });
@@ -37,7 +38,7 @@ export default function CategoryPage() {
   return (
     <div className="h-auto w-full md:max-w-[95%] lg:max-w-full xl:max-w-[1300px] 2xl:max-w-[1440px] font-poppins bg-white">
       {/* Banner */}
-      <div className="w-full relative h-[245px] bg-blue-600">
+      <div className="w-full relative h-64 hidden lg:block lg:h-[245px] bg-blue-600">
         <div className="absolute z-10 top-[20%] left-[15%] w-[461px] h-[94px] text-[31px]">
           Efficient and Durable Electronics
         </div>
@@ -46,7 +47,14 @@ export default function CategoryPage() {
           src="/banner/Rectangle%2025.png"
           fill
         />
-        <div className="w-[328px] left-[15%] items-end bottom-[5%] flex justify-center items-center h-[46px] absolute">
+        <div className="w-[336px] h-[228px] absolute top-[10%] right-[16%]">
+          <Image
+            fill
+            alt="atlaze electronics category image"
+            src="/banner/Group%203.png"
+          />
+        </div>
+        <div className="w-[328px] left-[15%] items-end bottom-[5%] flex justify-center h-[46px] absolute">
           <div className="w-[107.94px] flex gap-1 items-center h-full">
             <div className="w-[34px] h-[34px] rounded-full bg-[#FF9900]" />
             <h1 className="w-[68px] h-[40px] font-bold text-black">
@@ -130,9 +138,9 @@ export default function CategoryPage() {
   );
 }
 
-function ProductCard({ product }: { product: any }) {
+function ProductCard({ product }: { product: WooProduct }) {
   return (
-    <div className="border border-gray-200 font-poppins bg-white rounded-xl p-3 flex flex-col">
+    <div className="border border-gray-200 font-poppins bg-white rounded-xl flex flex-col">
       <div className="relative w-full aspect-square mb-3">
         <Image
           src={product.images?.[0]?.src || "/placeholder.png"}
@@ -141,43 +149,46 @@ function ProductCard({ product }: { product: any }) {
           className="object-cover rounded-lg"
         />
       </div>
-      <h2 className="lg:text-[15px] text-[14px] flex flex-col lg:flex-row justify-between font-medium text-black mb-1">
-        {`${product.name.slice(0, 18)}...`}
-        <span className="flex items-center">
-          {[...Array(5)].map((_, idx) => {
-            return (
-              <FaStar
-                className="text-[8px] text-[#FFD700] lg:text-[10px]"
-                id={`${idx}`}
-              />
-            );
-          })}
-        </span>
-      </h2>
-      <div className="lg:text-[12px] text-[10px] text-black/50">
-        Elica 60 cm 1200 m3/hr Filterless Autoclean K...
-      </div>
-      <div className="flex mt-3 lg:mt-6 flex-col lg:flex-row justify-between lg:items-center">
-        <div className="flex flex-col">
-          <div className="flex items-center gap-2">
-            <div className="w-[14.5px] relative h-[14.5px] lg:w-[20px] lg:h-[20px]">
-              <Image
-                src="/home/hero/Nigeria.png"
-                className="object-cover"
-                alt="nigeria logo"
-                fill
-              />
-            </div>
-            <span className="text-[15px] lg:text-[14px] text-[#6A00EF]">
-              {product.price}
-            </span>
-          </div>
-          <span className="text-[10px] text-black/50">300+ purchased</span>
+      <div className="px-3 w-full h-auto">
+        <h2 className="lg:text-[15px] text-[14px] flex flex-col lg:flex-row justify-between font-medium text-black mb-1">
+          {`${product.name.slice(0, 15)}...`}
+          <span className="flex items-center">
+            {[...Array(5)].map((_, idx) => {
+              return (
+                <FaStar
+                key={`${idx}`}
+                  className="text-[8px] text-[#FFD700] lg:text-[10px]"
+                  id={`${idx}`}
+                />
+              );
+            })}
+          </span>
+        </h2>
+        <div className="lg:text-[12px] text-[10px] text-black/50">
+          Elica 60 cm 1200 m3/hr Filterless Autocl...
         </div>
-        <button className="lg:mt-auto mt-2 w-full lg:w-[110px] text-[10px] bg-[#6A00EF] text-white py-[8px] rounded-[24px] hover:bg-purple-700 items-center gap-1 justify-center flex transition">
-          <GoPlus className="text-2xl" />
-          Add to Cart
-        </button>
+        <div className="flex mt-3 lg:mt-6 flex-col lg:flex-row justify-between lg:items-center">
+          <div className="flex flex-col">
+            <div className="flex items-center gap-2">
+              <div className="w-[14.5px] relative h-[14.5px] lg:w-[20px] lg:h-[20px]">
+                <Image
+                  src="/home/hero/Nigeria.png"
+                  className="object-cover"
+                  alt="nigeria logo"
+                  fill
+                />
+              </div>
+              <span className="text-[12px] lg:text-[14px] text-[#6A00EF]">
+                {`${product.price.slice(0, 10)}...`}
+              </span>
+            </div>
+            <span className="text-[10px] text-black/50">300+ purchased</span>
+          </div>
+          <button className="lg:mt-auto mt-2 w-full lg:w-[110px] text-[10px] bg-[#6A00EF] text-white py-[6px] rounded-[24px] hover:bg-purple-700 items-center gap-1 justify-center flex transition">
+            <GoPlus className="text-2xl" />
+            Add to Cart
+          </button>
+        </div>
       </div>
     </div>
   );
