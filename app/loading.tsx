c0c26b523a4f75097109cf052719b11a6dd6fa4d
Metryx <157503371+michee2x@users.noreply.github.com>
2025-10-27 18:42:14 +0100

    Update loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@
 
 export default function Loading() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
+    <div className="w-screen inset-0 h-screen flex items-center justify-center">
       <span className="loading loading-spinner loading-xl text-primary"></span>
     </div>
   );
