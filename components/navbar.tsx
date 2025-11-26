937ebc70b08202bf9bb09112eb397023037fb79d
michee2x <michaelisraelmike@gmail.com>
2025-11-26 18:54:09 +0100

    fixed nav getintials type error

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -20,12 +20,13 @@ const NavBar = ({ showCategories }: { showCategories?: boolean }) => {
   const { session, isLoading } = useAuth(); // Access session from context
   const [imageLoaded, setImageLoaded] = useState(false); // Track if the image has loaded
 
-  const getInitials = (name?: string) => {
-    if (!name) return "";
-    const parts = name.trim().split(" ");
-    if (parts.length === 1) return parts[0].slice(0, 2).toUpperCase();
-    return (parts[0][0] + parts[1][0]).toUpperCase();
-  };
+  const getInitials = (name: string | null | undefined): string => {
+  if (!name) return "";
+  const parts = name.trim().split(" ");
+  if (parts.length === 1) return parts[0].slice(0, 2).toUpperCase();
+  return (parts[0][0] + parts[1][0]).toUpperCase();
+};
+
 
   useEffect(() => {
     if (session?.user?.image) {
